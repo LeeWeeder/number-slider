5b467e29259579007ca4906148aa4d34c109d2fd
LeeWeeder <ljmaq239@gmail.com>
2024-07-15 19:03:46 +0800

    Add `onTextFieldValueChange` callback parameter

--- app/src/main/java/com/leeweeder/numberslider/MainActivity.kt
+++ app/src/main/java/com/leeweeder/numberslider/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.leeweeder.numberslider
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -24,6 +25,8 @@ class MainActivity : ComponentActivity() {
                     }
                     NumberSlider(value = weight.floatValue, onValueChange = {
                         weight.floatValue = it
+                    }, onTextFieldValueChange = {
+                        Log.d("TextFieldValue", it)
                     })
                 }
             }

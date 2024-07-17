26be203ecb2baefc4c891e7d8d622c4957098ae6
LeeWeeder <ljmaq239@gmail.com>
2024-07-17 16:26:50 +0800

    Add documentation

--- number-slider/src/main/java/com/leeweeder/numberslider/model/IndicatorProperties.kt
+++ number-slider/src/main/java/com/leeweeder/numberslider/model/IndicatorProperties.kt
@@ -2,7 +2,14 @@ package com.leeweeder.numberslider.model
 
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import com.leeweeder.numberslider.NumberSlider
 
+/**
+ * Holds properties for customizing the appearance of value indicators in [NumberSlider].
+ *
+ * @param defaultStyle [TextStyle] for non-selected value indicators.
+ * @param selectedStyle [TextStyle] for the selected value indicator.
+ * */
 data class IndicatorProperties(
     val defaultStyle: TextStyle = TextStyle.Default,
     val selectedStyle: TextStyle = defaultStyle.copy(fontWeight = FontWeight.Bold)

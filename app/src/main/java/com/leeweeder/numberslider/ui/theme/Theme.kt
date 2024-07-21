53224e79aa963fdce0ffce11fcc7c143a1d364b9
LeeWeeder <ljmaq239@gmail.com>
2024-07-21 11:39:07 +0800

    add-license-headers

--- app/src/main/java/com/leeweeder/numberslider/ui/theme/Theme.kt
+++ app/src/main/java/com/leeweeder/numberslider/ui/theme/Theme.kt
@@ -1,7 +1,23 @@
+/*
+ * Number Slider Jetpack Compose. A customizable number slider component for Jetpack Compose, allowing users to select numeric values within a defined range.
+ * Copyright (C) 2024 LeeWeeder
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.leeweeder.numberslider.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -41,7 +57,7 @@ fun NumberSliderTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }

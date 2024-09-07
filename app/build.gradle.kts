92002557950486967e79e65ab1bfa0ee82d6c35b
LeeWeeder <ljmaq239@gmail.com>
2024-09-07 15:55:33 +0800

    Revert `isMinifyEnabled` to false

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
